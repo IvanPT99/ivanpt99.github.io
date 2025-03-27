149a0547e72ff072bc0c6a59b6f4be16bfb3a66f
Ivan PT. <ivanperezt1999@gmail.com>
2025-03-27 01:23:57 +0000

    Presentation

---
+++ dictionary.js
@@ -0,0 +1,26 @@
+const translations = {
+    es: {
+        "nav-about": "Sobre mí",
+        "nav-projects": "Proyectos",
+        "nav-contact": "Contacto",
+        "about-title": "Sobre mí",
+        "about-text": "Soy un desarrollador Full-Stack con experiencia en Java, AEM y Vue.js.",
+        "projects-title": "Proyectos",
+        "project-1": "Soulsdle - Un minijuego basado en Worldle con la saga Souls",
+        "project-2": "Gestor de facturación con Java y Spring Boot",
+        "contact-title": "Contacto",
+        "contact-text": "📫 <a href='mailto:tuemail@example.com'>Contáctame</a>",
+    },
+    en: {
+        "nav-about": "About Me",
+        "nav-projects": "Projects",
+        "nav-contact": "Contact",
+        "about-title": "About Me",
+        "about-text": "I'm a Full-Stack developer with experience in Java, AEM, and Vue.js.",
+        "projects-title": "Projects",
+        "project-1": "Soulsdle - A minigame based on the Souls saga",
+        "project-2": "Invoice manager with Java and Spring Boot",
+        "contact-title": "Contact",
+        "contact-text": "📫 <a href='mailto:tuemail@example.com'>Contact me</a>",
+    }
+};
